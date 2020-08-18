76cf14b25fe3b6debed1960e2295050b4c98f819
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2020-08-17 21:35:25 -0300

    Vehicle object created

---
+++ model/vehicle.js
@@ -0,0 +1,9 @@
+class Vehicle {
+    constructor({ brand, model, price }) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+}
+
+module.exports = Vehicle;
